4165deea1c652d2e9322e7f2fefc2269b96a54de
Dzianis Bogdan <bogdan.den@gmail.com>
2021-10-18 12:35:56 +0300

    feat: fruit class

---
+++ src/Fruit.lua
@@ -0,0 +1,24 @@
+Fruit = Class {}
+
+function Fruit:init(world, x, y, type)
+    self.world = world
+    self.x = x
+    self.y = y
+    self.size = 32
+    self.texture = gTextures['apple']
+    self.collider = world:newCircleCollider(self.x + self.size / 2,
+                                            self.y + self.size / 2,
+                                            self.size / 2)
+    self.collider:setCollisionClass('Items')
+    self.collider:setType('static')
+    self.collider:setObject(self)
+    self.grid =
+        anim8.newGrid(self.size, self.size, self.texture:getDimensions())
+    self.animation = anim8.newAnimation(self.grid('1-17', 1), 0.06)
+end
+
+function Fruit:update(dt) self.animation:update(dt) end
+
+function Fruit:render() self.animation:draw(self.texture, self.x, self.y) end
+
+function Fruit:destroy() print('destroy') end
