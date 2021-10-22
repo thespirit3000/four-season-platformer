15df921b681d6d465013aeb69c0138b1efd8340a
Dzianis Bogdan <bogdan.den@gmail.com>
2021-10-22 14:03:47 +0300

    feat: bee player querry

--- src/Enemies/BeeEnemy.lua
+++ src/Enemies/BeeEnemy.lua
@@ -7,7 +7,19 @@ function BeeEnemy:init(world, x, y)
     self.state = 'idle'
     self.hit = false
 end
-function BeeEnemy:update(dt) self.animation:update(dt, self.state) end
+function BeeEnemy:update(dt)
+    local px, py = self.collider:getPosition()
+    local playerQ = self.world:queryRectangleArea(px - self.width, py,
+                                                  2 * self.width,
+                                                  VIRTUAL_HEIGHT - py,
+                                                  {'Player'})
+    if #playerQ > 0 then
+        self.state = 'attack'
+    else
+        self.state = 'idle'
+    end
+    self.animation:update(dt, self.state)
+end
 function BeeEnemy:render()
     local px, py = self.collider:getPosition()
     self.animation:render(self.state, px, py, -1)
