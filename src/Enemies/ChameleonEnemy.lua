f3741fd8ed4de1f31bc080e3b28076550e56b863
Dzianis Bogdan <bogdan.denv@gmail.com>
2021-10-23 10:37:46 +0300

    feat: chameleon enemy init

---
+++ src/Enemies/ChameleonEnemy.lua
@@ -0,0 +1,4 @@
+ChameleonEnemy = Class {}
+function ChameleonEnemy:init(world, x, y) EnemyBase.init(self, world, x, y) end
+function ChameleonEnemy:update() end
+function ChameleonEnemy:render() end
