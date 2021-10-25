58a17b1521d88beb69013c7250847cc08b439587
Dzianis Bogdan <bogdan.denv@gmail.com>
2021-10-25 22:51:04 +0300

    feat: chameleon enemy

--- src/Enemies/ChameleonEnemy.lua
+++ src/Enemies/ChameleonEnemy.lua
@@ -1,4 +1,12 @@
+local enemySpeed = 80
 ChameleonEnemy = Class {}
-function ChameleonEnemy:init(world, x, y) EnemyBase.init(self, world, x, y) end
-function ChameleonEnemy:update() end
+function ChameleonEnemy:init(world, x, y)
+    EnemyBase.init(self, world, x, y)
+    self.animation = ChameleonAnimation()
+    self.state = 'run'
+    self.direction = 1
+    self.speed = 80
+    self.hit = false
+end
+function ChameleonEnemy:update() local px, py = self.collider:getPosition() end
 function ChameleonEnemy:render() end
