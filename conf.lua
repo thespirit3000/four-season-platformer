c2a607417053ba9d79ee6f9628b60809fda0057c
Dzianis Bogdan <bogdan.denv@gmail.com>
2021-11-28 14:14:04 +0300

    feat: clean proj

--- conf.lua
+++
@@ -1,1 +0,0 @@
-function love.conf(t) t.console = true end
--- src/states/HightScoresState.lua
+++
@@ -1,5 +0,0 @@
-HightScoresState = Class {__includes = BaseState}
-function HightScoresState:update(dt)
-end
-function HightScoresState:render()
-end
